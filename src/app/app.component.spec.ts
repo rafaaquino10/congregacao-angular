288b575abd82586bb93f6c39ccd18a20de191750
rafaaquino10 <rafa.aquino10@gmail.com>
2024-10-01 09:42:13 -0300

    chore: rename project.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'congregation-manager'`, () => {
+  it(`should have as title 'congregacao-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('congregation-manager');
+    expect(app.title).toEqual('congregacao-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('congregation-manager app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('congregacao-angular app is running!');
   });
 });
