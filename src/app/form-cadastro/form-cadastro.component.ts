b3360e33a5baa4560ddc265945c36e0260092806
rafaaquino10 <rafa.aquino10@gmail.com>
2024-10-05 17:53:58 -0300

    feat(form-cadastro): creating

--- src/app/form-cadastro/form-cadastro.component.ts
+++ src/app/form-cadastro/form-cadastro.component.ts
@@ -6,16 +6,68 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-cadastro.component.scss']
 })
 export class FormCadastroComponent {
-  batizado: boolean = false;
-  genero: string = '';
+  form = {
+    nome: '',
+    nomeCompleto: '',
+    genero: '',
+    dataNascimento: '',
+    batizado: false,
+    dataBatismo: '',
+    privilegio: '',
+    pioneiro: '',
+    ungido: false,
+    grupo: '',
+    logradouro: '',
+    numero: '',
+    complemento: '',
+    bairro: '',
+    cep: '',
+    cidade: '',
+    uf: '',
+    telefone: '',
+    email: '',
+    emergNome: '',
+    emergParentesco: '',
+    emergTelefone: '',
+    emergEmail: ''
+  };
 
-  toggleDataBatismoField() {
-    // A lógica para alternar o campo de Data de Batismo já está sendo gerenciada pelo Angular através do binding.
-    // O Angular automaticamente controla a exibição com base no valor de `batizado`.
+  constructor() { }
+
+  onBatizadoChange() {
+    if (!this.form.batizado) {
+      this.form.dataBatismo = '';
+    }
   }
 
-  togglePrivilegioField() {
-    // A lógica para alternar o campo de Privilégio já está sendo gerenciada pelo Angular através do binding.
-    // O Angular automaticamente controla a exibição com base no valor de `genero`.
+  onGeneroChange() {
+    if (this.form.genero !== 'masculino') {
+      this.form.privilegio = '';
+    }
+  }
+
+  formatarTelefone(): void {
+    const telefoneInput = document.getElementById('telefone') as HTMLInputElement;
+    let valor = telefoneInput.value;
+  
+    // Remove todos os caracteres que não sejam números
+    valor = valor.replace(/\D/g, '');
+  
+    // Aplica a máscara conforme o tamanho do valor
+    if (valor.length > 10) {
+      // Formato (99) 99999-9999
+      valor = valor.replace(/^(\d{2})(\d{5})(\d{4})$/, '($1) $2-$3');
+    } else if (valor.length > 6) {
+      // Formato (99) 9999-9999
+      valor = valor.replace(/^(\d{2})(\d{4})(\d{0,4})$/, '($1) $2-$3');
+    } else if (valor.length > 2) {
+      // Formato parcial (99) 9999
+      valor = valor.replace(/^(\d{2})(\d{0,5})$/, '($1) $2');
+    } else {
+      // Formato parcial (99
+      valor = valor.replace(/^(\d{0,2})$/, '($1');
+    }
+  
+    telefoneInput.value = valor;
   }
 }
